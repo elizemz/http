c7f2407e5ac024236d5e547035fd7fa1a54f67e7
Билгүүдэй Бүдшир <HP231031@G301-CC0178.local>
2023-10-25 18:23:43 +0800

    done

--- app.js
+++ app.js
@@ -1,46 +1,57 @@
 const express = require("express");
+// const { v5: uuid } = require("uuid");
 const fs = require("fs");
 
+const PORT = 3000;
 const app = express();
 
-app.
+app.use(express.json());
 
-const users = [
-	{ id: 1, username: "Ben", age: 10 },
-	{ id: 2, username: "Ten", age: 21 },
-	{ id: 3, username: "Hes", age: 23 },
-	{ id: 4, username: "Akid", age: 20 },
-];
-
-app.get("/", (req, res) => {
-	const a = fs.readFileSync("test.txt", { encoding: "utf-8" });
-	const count = content.split(" ").length;
-	res.send("hello express");
+app.get("/api/users", (req, res) => {
+	console.log("Get all users");
+	res.status(200).json({ message: "get_success" });
 });
 
-app.get("/about", (req, res) => {
-	res.status(200).json({ name: "Naraa", age: 29 });
+app.post("/api/users", (req, res) => {
+	console.log("Create new users", req.body);
+	const newUser = { ...req.body };
+
+	const { users } = JSON.parse(
+		fs.readFileSync("users.json", { encoding: "utf-8" })
+	); //{users: []}
+
+	users.push(newUser);
+
+	fs.writeFileSync("users.json", JSON.stringify({ users }), {
+		encoding: "utf-8",
+	});
+	res.status(200).json({ message: "post_success" });
 });
 
-app.get("/wordCount", (req, res) => {
-	const a = fs.readFileSync("test.txt", { encoding: "utf-8" });
-	const count = content.split(" ").length;
-	res.send("Count" + count);
+app.put("/api/users/:id", (req, res) => {
+	console.log("Update user by id");
+	res.status(200).json({ message: "put_success" });
 });
 
-app.get("/users/:userId", (req, res) => {
-	const { userId }= req.params;
-  console.log("UI", userId);
-	const findUser = users.filter((user)=> user.id=== Number(userId));
-  console.log("FU", findUser);
-	if (findUser.length === 0) {
-		res.status(404).json({ message: "Not Found"});
+app.delete("/api/users/:id", (req, res) => {
+	console.log("Delete user by id");
+	const { id } = req.params;
+	const { users } = JSON.parse(
+		fs.readFileSync("users.json", { encoding: "utf-8" })
+	);
+
+	const index = users.findIndex((el) => el.id === userId,
+	if (index < 0 ){
+		res.status(400).json({message: `cannot find ${userId}`})
 	} else{
+		res
+	});
+	users.splice(index, 1);
 
-  }
-	res.json(findUser);
+	fs.writeFileSync("users.json", JSON.stringify({ users }), {
+		encoding: "utf-8",
+	});
+	res.status(200).json({ message: "delete_success" });
 });
 
-// JSON.stringify();
-
-app.listen(3000, () => console.log("server listening at 3000 port"));
+app.listen(PORT, () => console.log("server listening at 3000 port"));
